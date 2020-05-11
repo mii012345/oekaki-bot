7806cfcd926dcf3bef8f8372aa8cbf5e00e70863
mii <mii012345@outlook.jp>
2020-05-11 22:04:05 +0900

    fix

--- main.py
+++ main.py
@@ -23,14 +23,10 @@ async def on_message(message):
         await message.channel.send('お絵描き始まるよ')
     if_add = re.compile(u"!add (.+) (.+)").search(message.content)
     if if_add:
-        oemori.add_word(if_add.group(1), if_add.group(2))
-        await message.channel.send('%sを%sで追加しました！'%(if_add.group(1), if_add.group(2)))
-        '''
         try:
             oemori.add_word(if_add.group(1), if_add.group(2))
             await message.channel.send('%sをレベル%sで追加しました！'%(if_add.group(1), if_add.group(2)))
         except:
             await message.channel.send('エラー:追加できませんでした')
-            '''
 
 client.run(Token)
